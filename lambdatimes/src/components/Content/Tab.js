ff906cd6113465668b9fa08ab30696b25713df88
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-22 13:39:39 -0500

    converted Tab to styled component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TabDiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+const dynamicStyle = {
+  backgroundColor:'#fff',
+  color:'#333',
+  border:'2px solid #333'
+}
 
 const Tab = props => {
   return (
-    <div
-      className={`tab${props.tab === props.selectedTab ? ' active-tab' : ''}`}
-      onClick={() => props.selectTabHandler(props.tab)}
-    >
+    <TabDiv onClick={() => props.selectTabHandler(props.tab)}
+    style={props.tab===props.selectedTab ? dynamicStyle : {}}>
       {props.tab.toUpperCase()}
-    </div>
+    </TabDiv>
   );
 };
 
