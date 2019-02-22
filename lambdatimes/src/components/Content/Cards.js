b71233e5dff163d4fe02164f44119940f17bb425
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-22 13:57:36 -0500

    converted Card and Cards to styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+}
+@media (min-width: 1200px) {
+  width: 1200px;
+`
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardsContainer>
       {props.cards.map( input => <Card card={input} key={input.headline}/>)}
-    </div>
+    </CardsContainer>
   )
 }
 
